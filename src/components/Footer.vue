7d9192c15076ce1b42c45fc8dea1d517e98ba1b4
farisfian06 <farisfian06@gmail.com>
2025-04-20 00:38:02 +0700

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,29 @@
 <template>
-  <div>
-    <h1 class="bg-slate-400 text-center">Footer</h1>
+  <div class="bg-primary py-10">
+    <div class="container">
+      <div class="flex gap-4 justify-center">
+        <div class="flex items-center gap-4 w-full">
+          <img src="/public/Logo.png" alt="" class="ml-auto" />
+          <div class="text-white w-fit">
+            <h1 class="font-bold text-6xl">TEMAN</h1>
+            <h3 class="text-lg">
+              Teman Bicara & Konseling Mahasiswa FILKOM UB
+            </h3>
+          </div>
+        </div>
+        <div class="border-2 rounded"></div>
+        <div class="text-white w-full py-12">
+          <h1 class="text-2xl font-bold">Informasi Kontak</h1>
+          <h2 class="text-lg mt-4">Phone</h2>
+          <p>+62 811-1109-304</p>
+          <h2 class="text-lg mt-4">Alamat</h2>
+          <p>
+            Fakultas Ilmu Komputer Universitas Brawijaya Jalan Veteran Nomor 8,
+            Lowokwaru, Malang, Jawa Timur 65145
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
