2c92ef5fb92bd3d3b488b811650667ba80ae3bcc
farisfian06 <farisfian06@gmail.com>
2025-04-21 20:17:06 +0700

    Update router.js

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import App from "@/App.vue";
 import AboutPage from "../pages/AboutPage.vue";
 import LandingPage from "../pages/LandingPage.vue";
+import AritkelPage from "../pages/AritkelPage.vue";
+import LoginPage from "../pages/LoginPage.vue";
 
 const routes = [
   {
@@ -9,7 +11,19 @@ const routes = [
     component: LandingPage,
   },
   {
-    path: "/about",
+    path: "/chatbot",
+    component: AboutPage,
+  },
+  {
+    path: "/login",
+    component: LoginPage,
+  },
+  {
+    path: "/artikel",
+    component: AritkelPage,
+  },
+  {
+    path: "/konseling",
     component: AboutPage,
   },
 ];
