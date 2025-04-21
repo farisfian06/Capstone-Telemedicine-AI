bb0409bef2ba61575ebf2c0832f1a97cf3212af0
farisfian06 <farisfian06@gmail.com>
2025-04-21 20:16:59 +0700

    Create AritkelPage.vue

---
+++ src/pages/AritkelPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <router-view></router-view>
+  <ArtikelSection />
+</template>
+
+<script setup>
+import ArtikelSection from "../components/sections/ArtikelPage/ArtikelSection.vue";
+</script>
+
+<style lang="scss" scoped></style>
