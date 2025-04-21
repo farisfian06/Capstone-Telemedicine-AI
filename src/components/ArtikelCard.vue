221bb2e9af70835a0a9f1bda47f8a62b05881f1d
farisfian06 <farisfian06@gmail.com>
2025-04-21 20:16:33 +0700

    Update ArtikelCard.vue

--- src/components/ArtikelCard.vue
+++ src/components/ArtikelCard.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="w-full border-2 rounded-lg">
-    <img
-      :src="imgSrc"
-      :alt="imgAlt"
-      class="w-full object-cover relative top-0"
-    />
-    <div class="px-8 py-4 space-y-2">
-      <h3>{{ title }}</h3>
-      <p>{{ description }}</p>
+  <router-link to="/artikel">
+    <div class="w-full border-2 rounded-lg shadow-md">
+      <img
+        :src="imgSrc"
+        :alt="imgAlt"
+        class="w-full object-cover relative top-0"
+      />
+      <div class="px-8 py-4 space-y-2">
+        <h3>{{ title }}</h3>
+        <p>{{ description }}</p>
+      </div>
     </div>
-  </div>
+  </router-link>
 </template>
 
 <script setup>
