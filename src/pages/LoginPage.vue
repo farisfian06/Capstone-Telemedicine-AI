bcd1ea48ceddbe3d39cccddc131fb4db0dac1e1e
farisfian06 <farisfian06@gmail.com>
2025-04-21 20:17:03 +0700

    Create LoginPage.vue

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-view></router-view>
+  <section
+    class="h-screen"
+    :style="`background-image: url(${sectionBg}); background-size: cover; background-position: center;`"
+  >
+    <div class="container h-full flex items-center justify-center">
+      <div class="flex max-w-xl w-full gap-6 items-center">
+        <img src="/Logo.png" alt="" class="object-contain w-1/3" />
+        <LoginForm />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import sectionBg from "@/assets/images/LoginPage/Login Page.png";
+import LoginForm from "../components/LoginPage/LoginForm.vue";
+</script>
