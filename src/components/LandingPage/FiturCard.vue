211503fdd372cab6377458341f2d637649f1ab78
farisfian06 <farisfian06@gmail.com>
2025-04-21 20:16:41 +0700

    Update FiturCard.vue

--- src/components/LandingPage/FiturCard.vue
+++ src/components/LandingPage/FiturCard.vue
@@ -2,7 +2,7 @@
   <div
     class="flex flex-col w-full h-fit gap-2 justify-center items-center text-center text-white"
   >
-    <img :src="imgSrc" :alt="imgAlt" class="w-20" />
+    <img :src="imgSrc" :alt="imgAlt" class="w-20" draggable="false" />
     <h2 class="text-secondary text-2xl font-bold">{{ title }}</h2>
     <p>{{ description }}</p>
   </div>
