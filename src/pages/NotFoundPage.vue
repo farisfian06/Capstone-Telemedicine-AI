429c6711fa1f13de7d1d5ce38f2abdeb97422bd6
farisfian06 <farisfian06@gmail.com>
2025-04-25 20:53:34 +0700

    Create NotFoundPage.vue

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="not-found">
+    <h1>404 - Page Not Found</h1>
+    <p>Halaman yang kamu cari tidak ditemukan.</p>
+    <router-link to="/">Kembali ke Beranda</router-link>
+  </div>
+</template>
+<script setup></script>
+
+<style lang="scss" scoped></style>
