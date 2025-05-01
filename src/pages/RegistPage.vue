d16f5db0289f323ac4389da020618efa2e91c0c8
farisfian06 <farisfian06@gmail.com>
2025-05-01 14:22:27 +0700

    Create RegistPage.vue

---
+++ src/pages/RegistPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <router-view></router-view>
+  <section
+    class="h-screen"
+    :style="`background-image: url(${sectionBg}); background-size: cover; background-position: center;`"
+  >
+    <div class="container h-full flex items-center justify-center">
+      <div class="flex max-w-xl w-full gap-6 items-center">
+        <img src="/Logo.png" alt="" class="object-contain w-1/3" />
+        <RegistForm />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import sectionBg from "@/assets/images/LoginPage/Login Page.png";
+import RegistForm from "../components/RegistPage/RegistForm.vue";
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+// Check if user is already logged in when component mounts
+onMounted(() => {
+  const token = localStorage.getItem("user_token");
+
+  // If token exists, redirect to home page
+  if (token) {
+    router.push("/");
+  }
+});
+</script>
